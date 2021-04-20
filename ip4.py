86d08578e0fc23cbb0055a1ced4cbe149c27f4ef
RZ <30381083+w311ang@users.noreply.github.com>
2021-04-20 13:35:12 +0800

    Update ip4.py

--- ip4.py
+++ ip4.py
@@ -8,10 +8,5 @@ output=''
 for one in result1:
   output+='address=/%s/::\n'%one
 #print(output)
-com=requests.get('https://data.iana.org/TLD/tlds-alpha-by-domain.txt').text
-com=re.sub('#.*\n','',com).split('\n')
-com.remove('')
-for one in com:
-  output+='address=/%s/::\n'%one
-with open('ip4.conf','w') as f:
+with open('ipv4only.conf','w') as f:
   f.write(output)
