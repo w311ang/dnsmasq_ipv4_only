4924a72ce75a53422bf2e3b2e5f615eb3c861720
RZ <30381083+w311ang@users.noreply.github.com>
2021-04-21 20:10:42 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -4,7 +4,7 @@ import re
 ori=requests.get('https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist.conf').text
 
 with open('ownlist.txt') as f:
-  add=f.read()
+  global ori
   ori+='\n'
   for line in f:
     ori+='server=/%s/127.0.0.1#5353\n'%line
