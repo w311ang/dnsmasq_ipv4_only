05d0574ef5fcc08057dc3140b0fa2b6e8cf68ad0
RZ <30381083+w311ang@users.noreply.github.com>
2021-04-21 19:59:51 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,14 +1,18 @@
 import requests
 import re
 
-with open('gfwlist.conf') as f:
-  o=f.read()
-#o=requests.get('https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist.conf').text
+ori=requests.get('https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist.conf').text
+
+with open('ownlist.txt') as f:
+  add=f.read()
+  ori+='\n'
+  for line in f:
+    ori+='server=/%s/127.0.0.1#5353\n'%line
 pattern = re.compile(r'(?<=/).*?(?=/)')
 result1 = pattern.findall(o)
 output=''
 for one in result1:
   output+='address=/%s/::\n'%one
 #print(output)
-with open('ipv4only.conf','w') as f:
-  f.write(output)
+with open('output/gfwipv4.conf','w') as f:
+  f.write(ori+'\n'+output)
