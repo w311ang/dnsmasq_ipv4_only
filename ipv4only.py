778d8af76aa09c9588117000e7ad6e64fe7463e4
RZ <30381083+w311ang@users.noreply.github.com>
2021-04-24 00:54:57 +0800

    Update ipv4only.py

--- ipv4only.py
+++ ipv4only.py
@@ -7,6 +7,7 @@ com=requests.get('https://data.iana.org/TLD/tlds-alpha-by-domain.txt').text
 com=re.sub('#.*\n','',com).split('\n')
 com.remove('')
 for one in com:
+  output+='server=/%s/127.0.0.1#5353\n'%one
   output+='address=/%s/::\n'%one
 with open('docs/ipv4only.conf','w') as f:
   f.write(output)
