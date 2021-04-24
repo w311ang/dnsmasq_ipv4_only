65e1afd7ec547eec564612b45045be737fdb6b9e
RZ <30381083+w311ang@users.noreply.github.com>
2021-04-24 17:02:23 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -2,7 +2,6 @@ import requests
 import re
 
 ori=requests.get('https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist.conf').text
-
 with open('ownlist.txt') as f:
   ori+='\n'
   for line in f:
@@ -11,8 +10,16 @@ with open('ownlist.txt') as f:
     ori+='server=/%s/127.0.0.1#5353\n'%line
 pattern = re.compile(r'(?<=/).*?(?=/)')
 result1 = pattern.findall(ori)
+
+com=requests.get('https://data.iana.org/TLD/tlds-alpha-by-domain.txt').text
+com=re.sub('#.*\n','',com).split('\n')
+com.remove('')
+
 output=''
 for one in result1:
+  output+='address=/%s/::\n'%one
+for one in com:
+  output+='server=/%s/211.142.211.124\n'%one
   output+='address=/%s/::\n'%one
 #print(output)
 with open('docs/gfwipv4.conf','w') as f:
