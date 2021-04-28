3f4a2772b8e1a0191fadb5e31298075a9693ca03
RZ <30381083+w311ang@users.noreply.github.com>
2021-04-28 20:43:01 +0800

    Update updateList.py

--- updateList.py
+++ updateList.py
@@ -1,7 +1,9 @@
 import requests
 import os
+import time
 
 pw=os.getenv('pw')
+cmd='cat /etc/storage/gfwlist/dnsmasq_gfwlist.conf'
 
 proxies = {
     'http': 'socks5://localhost:1080',
@@ -12,3 +14,10 @@ session.proxies.update(proxies)
 
 session.post('http://192.168.2.1/Shadowsocks_action.asp',data='connect_action=Update_gfwlist',auth=('w311ang',pw))
 session.get('http://192.168.2.1/Logout.asp')
+
+session.post('http://192.168.2.1/apply.cgi',data={'action_mode':' SystemCmd ','current_page':'console_response.asp','next_page':'console_response.asp','SystemCmd':cmd},auth=('w311ang',pw))
+rep=session.get('http://192.168.2.1/console_response.asp',auth=('w311ang',pw)).text
+with open('docs/gfwipv4.conf') as f:
+  be=f.read()
+if not rep==be:
+  raise Exception('未能同步')
